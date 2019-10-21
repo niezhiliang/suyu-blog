628747dbbb82f7217172e8ba9c177a611126dc78
niezhiliang <1132263176@qq.com>
2019-10-21 12:59:29 +0800

    m

--- publish.sh
+++ publish.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd blog-temp
+
+scp *.md root@www.isuyu.cn:/usr/local/suyu-blog/source/_posts
+
+git pull
+
+cd blog-temp
+
+mv *.md ../source/_posts
+
+
 git status
 
 git add .
@@ -7,16 +18,6 @@ git add .
 git commit -m 'm'
 
 git push
-
-
-cd blog-temp
-
-scp *.md root@www.isuyu.cn:/usr/local/suyu-blog/source/_posts
-
-git pull
-
-
-cd blog-temp
 
 rm -f -r *.md
 
